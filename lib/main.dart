ed86b0c8cbe0c20ee8a57416eb564215fdb47dc2
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-04-14 12:40:49 +0900

    ci: update text again for preview CLI

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World from Preview!'),
+          child: Text('Hello World from Preview again!'),
         ),
       ),
     );
